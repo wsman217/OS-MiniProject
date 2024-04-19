5e3b885755707def674c172bc9ddd5300192bf2f
wsman217 <westonsublett1@hotmail.com>
2024-04-18 23:05:03 -0500

    Finished the project?

--- scheduler.c
+++ scheduler.c
@@ -10,6 +10,8 @@
 
 // sequence counter of next available thread identifier
 int nextTid = 0;
+int completionTime = 0;
+int counter = -1;
 
 Task *selectNextTask(Queue *queue, int scheduleAlgorithmSwitch);
 
@@ -33,14 +35,63 @@ void add(Queue *queue, char *name, int arrivalTime, int burst) {
 void schedule(Queue *queue, int scheduleAlgorithmSwitch) {
     Task *current;
 
-    // sanity checker
-    traverse(queue);
+    int queueSize = queue->size;
+
+    int completionTimeArray[queueSize];
+    int burstTimes[queueSize];
+    int arrivalTimes[queueSize];
+    char pids[queueSize][2];
 
     while (!isEmpty(queue)) {
+        counter++;
+
         current = selectNextTask(queue, scheduleAlgorithmSwitch);
 
+        if (current == NULL) {
+            return;
+        }
+
+        int arrivalTime = current->arrivalTime;
+        if (arrivalTime > completionTime) {
+            completionTime = current->arrivalTime + current->burst;
+            completionTimeArray[counter] = completionTime;
+        } else {
+            completionTime += current->burst;
+            completionTimeArray[counter] = completionTime;
+        }
+        burstTimes[counter] = current->burst;
+        arrivalTimes[counter] = arrivalTime;
+        pids[counter][0] = current->name[0];
+        pids[counter][1] = current->name[1];
         run(current, current->burst);
     }
+
+    printf("\n\n\n");
+    printf("PID | %15s | %13s | %12s | %14s", "Turnaround Time", "Response Time", "Waiting Time", "Response Ratio\n");
+
+    double rTT = 0;
+    double rWT = 0;
+    double rBT = 0;
+
+    for (int i = 0; i < queueSize; i++) {
+        double tt = completionTimeArray[i] - arrivalTimes[i];
+        double rt = (completionTimeArray[i] - burstTimes[i]) - arrivalTimes[i];
+        double wt = tt - burstTimes[i];
+        double rr = (wt + burstTimes[i]) / burstTimes[i];
+
+        rTT += tt;
+        rWT += wt;
+        rBT += burstTimes[i];
+
+        char name[3];
+        name[0] = pids[i][0];
+        name[1] = pids[i][1];
+
+        printf("%3s | %15.0f | %13.0f | %12.0f | %14.2f\n", name, tt, rt, wt, rr);
+    }
+
+    printf("\n\n%23s | %20s | %10s", "Average Turnaround Time", "Average Waiting Time", "Throughput\n");
+    printf("%23.2f | %20.2f | %10.2f\n", rTT / queueSize, rWT / queueSize, rBT / queueSize);
 }
 
 /**
